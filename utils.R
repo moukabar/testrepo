dd33bad273b425fe29195397851a5b37da487ba3
moukabar <robert.moukabary@gmail.com>
2017-12-30 14:25:24 +0100

    Streamlining projects

--- utils.R
+++
@@ -1,8 +0,0 @@
-#
-# Utility
-
-order_anum_vec <- function(anum_vec, ...) {
-  alpha <- gsub("([A-Z]+)([0-9]+)", "\\1", anum_vec)
-  num <- as.numeric(gsub("([A-Z]+)([0-9]+)", "\\2", anum_vec))
-  return(order(alpha, num, ...))
-}
